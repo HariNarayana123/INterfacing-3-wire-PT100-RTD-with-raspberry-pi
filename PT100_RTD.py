c1e96ef9697649e1a3b6fd49992716ebfea71f98
HariNarayana123 <62258694+HariNarayana123@users.noreply.github.com>
2023-04-02 08:56:22 +0530

    Update PT100_RTD.py

--- PT100_RTD.py
+++ PT100_RTD.py
@@ -1,3 +1,8 @@
+# install the Adafruit MAX31865 library to interact with the MAX31865 amplifier 
+# sudo pip3 install adafruit-circuitpython-max31865 // run this command on rspi
+
+
+
 import board
 import time
 import busio
@@ -5,8 +10,17 @@ import adafruit_max31865
 
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 cs = digitalio.DigitalInOut(board.CE0)
-sensor = adafruit_max31865.MAX31865(spi, cs, rtd_nominal=100, ref_resistor=430.0, wires=4)
+sensor = adafruit_max31865.MAX31865(spi, cs, rtd_nominal=100, ref_resistor=430.0, wires=3)
 
 while True:
+  try:  
     temperature = sensor.temperature
+    print(f"Temperature: {tempeparture}{chr(248)}C")
     time.sleep(0.2)
+  except KeyboardInterrupt:
+      print("user interrupted the programme")
+      sys.exit()
+  finally:
+      GPIO.cleanup()
+      spi.close()  
+        
